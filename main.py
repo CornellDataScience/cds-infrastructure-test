e2cce284cf6fdebe4d673a9dc4ab36f932057cc0
Emily Wang <esw77@cornell.edu>
2022-10-14 18:18:54 -0400

    Basic folder structure

---
+++ main.py
@@ -0,0 +1,1 @@
+print("Hahah I have torch")
---
+++ src/utils/generateJoke.py
@@ -0,0 +1,11 @@
+from collections import deque
+
+
+class JokeGenerator:
+  def __init__(self, name):
+    self.name = name
+    self.joke_list = deque()
+    # Adding elements to a queue
+    q.append('a')
+    q.append('b')
+    q.append('c')
