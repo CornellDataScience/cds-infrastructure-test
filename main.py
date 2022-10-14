76179fd6b829015fe945da61cfe943b094906ad8
Emily Wang <esw77@cornell.edu>
2022-10-14 18:34:32 -0400

    added basic import to main

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Hahah I have torch")
+from src.utils.generateJoke import JokeGenerator
+
+#print("Hahah I have torch")
+
+j = JokeGenerator("Emily")
+
+while(j.joke_list) :
+  print(j.getJoke())
+  userAns = input()
+  # output
+  print(j.generateResponse(userAns))
--- src/utils/generateJoke.py
+++ src/utils/generateJoke.py
@@ -7,6 +7,10 @@ class JokeGenerator:
     self.joke_list = deque()
     # Adding elements to a queue
     self.joke_list.append("What's 25*2?")
+    self.joke_list.append("What is 'an imitation of a song or artist, with deliberate exaggeration for comic effect' - but plural?")
   
   def getJoke(self):
     return self.joke_list.popleft()
+
+  def generateResponse(self, userAns):
+    return userAns + " nuts in your mouth! :P"
